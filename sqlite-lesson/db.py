c33fb982e427b0c82d992a03cbd28f3dac6c890d
Jack Lee <jack.lee.1981@pm.me>
2021-01-02 17:00:25 -0300

    Make minor corrections to SQLite lesson.

--- sqlite-lesson/db.py
+++ sqlite-lesson/db.py
@@ -4,10 +4,10 @@ conn = sqlite3.connect("data.db")
 
 cursor = conn.cursor()
 
-cursor.execute("DROP TABLE users")
+cursor.execute("DROP TABLE IF EXISTS users")
 
 create_table = "CREATE TABLE users ( \
-id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, \
+id INTEGER PRIMARY KEY NOT NULL, \
 username VARCHAR(255) NOT NULL UNIQUE, \
 password VARCHAR(255) NOT NULL \
 )"
@@ -15,10 +15,10 @@ password VARCHAR(255) NOT NULL \
 cursor.execute(create_table)
 
 users = [
-    (1, "jose", "123",),
-    (2, "rob", "123",)
+    ("jose", "123",),
+    ("rob", "123",)
 ]
-insert_query = "INSERT INTO users VALUES (?, ?, ?)"
+insert_query = "INSERT INTO users VALUES (NULL, ?, ?)"
 cursor.executemany(insert_query, users)
 
 select_query = "SELECT * FROM users"
